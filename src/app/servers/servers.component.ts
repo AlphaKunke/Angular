b1694f59e4311e8330220f6c2bb8a68e3d72d3ea
Kevin Kalfon <Kevin.Kalfon@qualitservices.tech>
2022-04-19 11:42:31 -0500

    Section 1

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = "Testserver";
+  username = '';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver2'];
+  displayDetails = false;
+  buttonClicks = [];
+
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    } ,2000);
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    console.log(event)
+    this.serverName = event.target.value;
+  }
+
+  onDisplayDetails() {
+    this.displayDetails = !this.displayDetails;
+    this.buttonClicks.push(this.buttonClicks.length + 1);
+
+  }
+
+} 
